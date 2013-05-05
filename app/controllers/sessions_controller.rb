0fd42bf09792dac3050ae22cf979983bcd171efb
Shane Rogers <rogerssh@tcd.ie>
2013-05-04 19:13:45 -0600

    successful session destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "Signed in!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Signed Out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blog::Application.routes.draw do
   resources :articles
   root to: 'articles#index'
-
+  # match 'auth/failure', redirect_to root_path
   match 'auth/:provider/callback', to: "sessions#create"
+  match 'signout', to: "sessions#destroy", as: "signout"
 end
